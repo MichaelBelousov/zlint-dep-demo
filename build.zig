3db13f3163467b9fb141e86d9abd633dbdda67f7
Michael Belousov <michael.belousov98@gmail.com>
2026-08-08 22:27:44 -0400

    start demo project

---
+++ build.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+const zlint = @import("zlint");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const mod = b.addModule("zlint_demo", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "zlint_demo",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zlint_demo", .module = mod },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_exe_tests.step);
+
+    const zlint_dep = b.dependency("zlint", .{
+        .custom_rules = &[_]std.Build.LazyPath{
+            b.path("./build.zig.zon"),
+        },
+    });
+    const run_lint = zlint.addRunLint(b, zlint_dep);
+    const lint_step = b.step("lint", "run zlint");
+    lint_step.dependOn(&run_lint.step);
+}
